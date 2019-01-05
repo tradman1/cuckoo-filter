56f6aaf4ff981a2ef05f3c3fac8a2fe8bbfb9d74
asajatovic <antonio.sajatovic>
2019-01-05 16:39:33 +0100

    Use Google C++ style

--- src/cuckoo_hashtable.h
+++ src/cuckoo_hashtable.h
@@ -18,17 +18,15 @@ class CuckooHashTable {
     // initialize buckets to be of size [n_buckets, bucket_size]
     buckets_.resize(n_buckets_, std::vector<uint16_t>(bucket_size));
   }
-  int NumBuckets() const {
-    return n_buckets_;
-  }
+  int NumBuckets() const { return n_buckets_; }
 
   // returns true if insertion was successful
   bool Insert(uint16_t fingerprint, int bucket_idx);
-  // returns true if fingerprint at given index in given bucket was succesfully deleted
+  // returns true if fingerprint at given index in given bucket was succesfully
+  // deleted
   bool Remove(uint16_t fingerprint, int bucket_idx);
   // returns true if the bucket at given index contains the fingerprint
   bool Contains(uint16_t fingerprint, int bucket_idx);
-
 };
 
 };  // namespace cuckoofilter
