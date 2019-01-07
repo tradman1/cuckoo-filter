80c79f54aef1538215ea55e3ec6b3df973c3716c
Antonio Šajatović <antonio.sajatovic@gmail.com>
2019-01-07 14:22:29 +0100

    Add insert with relocation

--- src/cuckoo_filter.h
+++ src/cuckoo_filter.h
@@ -3,30 +3,37 @@
 
 #include <string>
 #include "cuckoo_hashtable.h"
+#include "hashutil.h"
 
 namespace cuckoofilter {
 
 enum Status { Ok = 0, NotFound = 1, NotEnoughSpace = 2 };
 
-template <class InputType>
+const size_t MaxNumKicks = 500;
+
+template <class InputType, class HashType>
 class CuckooFilter {
   CuckooHashTable *table_;
+  HashType hasher_;
 
   inline void GenerateFingerprintIndices(const InputType &item,
                                          int *bucket_idx1, int *bucket_idx2,
                                          uint16_t *fingerprint) const {
     // partial-key cuckoo hashing
-    int bits_per_item = 4;    // how to define this dynamically?
-    const uint64_t hash = 0;  // calculate the real hash here
-    // generate fingerprint from item hash
+    const uint64_t hash = hasher_(item);
+    // fingerprint is a bit string derived from the item using a hash function
+    int bits_per_item = 4;  // TO-DO define this dynamically
     *fingerprint = hash & ((1ULL << bits_per_item) - 1);
-    *fingerprint += (fingerprint == 0);
-    // generate index1 from item hash
+    *fingerprint += (*fingerprint == 0);
+    // index1 = hash(item)
     *bucket_idx1 = (hash >> 32) & (table_->NumBuckets() - 1);
-    // generate index2 from index1 and fingerprint
+    // index2 = index1 xor hash(fingerprint)
+    *bucket_idx2 = AltIndex(*bucket_idx1, *fingerprint);
+  }
+
+  inline int AltIndex(const int index, const uint16_t fingerprint) const {
     // 0x5bd1e995 is the hash constant from MurmurHash2
-    *bucket_idx2 = (*bucket_idx1 ^ (*fingerprint * 0x5bd1e995)) &
-                   (table_->NumBuckets() - 1);
+    return (index ^ (fingerprint * 0x5bd1e995)) & (table_->NumBuckets() - 1);
   }
 
  public:
@@ -36,8 +43,8 @@ class CuckooFilter {
   Status Lookup(const InputType &item);
 };
 
-template <class InputType>
-Status CuckooFilter<InputType>::Insert(const InputType &item) {
+template <class InputType, class HashType>
+Status CuckooFilter<InputType, HashType>::Insert(const InputType &item) {
   uint16_t fingerprint;
   int bucket_idx1, bucket_idx2;
 
@@ -49,15 +56,19 @@ Status CuckooFilter<InputType>::Insert(const InputType &item) {
     return Ok;
   }
   // must relocate existing items
-  /*
-    TO-DO
-  */
+  for (uint32_t n = 0; n < kMaxCuckooCount; n++) {
+    fingerprint = table->SwapEntries(fingerprint, bucket_idx1);
+    bucket_idx1 = AltIndex(bucket_idx1, fingerprint);
+    if (table_->Insert(fingerprint, bucket_idx1)) {
+      return Ok;
+    }
+  }
   // HashTable is considered full
   return NotEnoughSpace;
 }
 
-template <class InputType>
-Status CuckooFilter<InputType>::Lookup(const InputType &item) {
+template <class InputType, class HashType>
+Status CuckooFilter<InputType, HashType>::Lookup(const InputType &item) {
   uint16_t fingerprint;
   int bucket_idx1, bucket_idx2;
 
@@ -72,8 +83,8 @@ Status CuckooFilter<InputType>::Lookup(const InputType &item) {
   return NotFound;
 }
 
-template <class InputType>
-Status CuckooFilter<InputType>::Delete(const InputType &item) {
+template <class InputType, class HashType>
+Status CuckooFilter<InputType, HashType>::Delete(const InputType &item) {
   uint16_t fingerprint;
   int bucket_idx1, bucket_idx2;
 
