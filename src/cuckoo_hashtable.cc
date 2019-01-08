729ed83649b75f70bf20aba6b86ad613e64aae19
tradman1 <tome.radman95@gmail.com>
2019-01-08 18:55:55 +0100

    Implement cuckoo_hashtable

---
+++ src/cuckoo_hashtable.cc
@@ -0,0 +1,45 @@
+#include <cstdint>
+#include <random>
+#include "cuckoo_hashtable.h"
+
+namespace cuckoofilter {
+
+bool CuckooHashTable::Insert(uint16_t fingerprint, int bucket_idx) {
+  if (buckets_[bucket_idx].size() >= bucket_size) {
+    return false;
+  }
+  buckets_[bucket_idx].insert(fingerprint);
+  return true;
+}
+
+bool CuckooHashTable::Remove(uint16_t fingerprint, int bucket_idx) {
+  std::unordered_set<uint16_t> bucket = buckets_[bucket_idx];
+  if(bucket.find(fingerprint) != bucket.end()) {
+    bucket.erase(fingerprint);
+    return true;
+  } else {  
+    return false;
+  }
+}
+
+bool CuckooHashTable::Contains(uint16_t fingerprint, int bucket_idx) {
+  return buckets_[bucket_idx].find(fingerprint) != buckets_[bucket_idx].end();
+}
+
+uint16_t CuckooHashTable::SwapEntries(uint16_t fingerprint, int bucket_idx) {
+  std::vector<uint16_t> bucket_cpy;
+  std::unordered_set<uint16_t> bucket = buckets_[bucket_idx];
+  std::copy(bucket.begin(), bucket.end(), bucket_cpy.begin());
+
+  // random generator
+  std::random_device rd;     // only used once to initialise (seed) engine
+  std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
+  std::uniform_int_distribution<int> uni(0, bucket.size() - 1); // guaranteed unbiased
+  int swap_idx = uni(rng);
+
+  uint16_t old_entry = bucket_cpy[swap_idx];
+  bucket.erase(old_entry);
+  bucket.insert(fingerprint);
+  return old_entry;
+}
+}
