1507e5f5299eb3d02fe7f26a6e10bbf0077f797b
tradman1 <tome.radman95@gmail.com>
2019-01-08 18:55:55 +0100

    Implement cuckoo_hashtable

--- src/cuckoo_hashtable.h
+++ src/cuckoo_hashtable.h
@@ -2,6 +2,7 @@
 #define CUCKOO_FILTER_CUCKOO_HASHTABLE_H_
 
 #include <vector>
+#include <unordered_set>
 
 namespace cuckoofilter {
 
@@ -10,13 +11,13 @@ class CuckooHashTable {
 
   int capacity_;
   int n_buckets_;
-  std::vector<std::vector<uint16_t>> buckets_;
+  std::vector<std::unordered_set<uint16_t>> buckets_;
 
  public:
   explicit CuckooHashTable(const int capacity) : capacity_(capacity) {
     n_buckets_ = capacity / bucket_size + 1;
     // initialize buckets to be of size [n_buckets, bucket_size]
-    buckets_.resize(n_buckets_, std::vector<uint16_t>(bucket_size));
+    buckets_.resize(n_buckets_, std::unordered_set<uint16_t>());
   }
   int NumBuckets() const { return n_buckets_; }
 
