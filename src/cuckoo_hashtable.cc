0e670dcbc1c004efe31155ef150e854d9eb61106
asajatovic <antonio.sajatovic>
2019-01-11 21:37:40 +0100

    Add proper code

--- src/cuckoo_hashtable.cc
+++ src/cuckoo_hashtable.cc
@@ -6,45 +6,46 @@ namespace cuckoofilter {
 
 bool CuckooHashTable::Insert(uint32_t fingerprint, size_t bucket_idx) {
   bucket_idx %= this->NumBuckets();
-  if (buckets_[bucket_idx].size() >= bucket_size) {
+  std::vector<uint32_t> bucket = buckets_[bucket_idx];
+
+  if (bucket.size() >= bucket_size) {
     return false;
   }
-  return buckets_[bucket_idx].insert(fingerprint).second;
+  bucket.push_back(fingerprint);
+  return true;
 }
 
 bool CuckooHashTable::Remove(uint32_t fingerprint, size_t bucket_idx) {
   bucket_idx %= this->NumBuckets();
-  std::unordered_set<uint32_t> bucket = buckets_[bucket_idx];
-  if (buckets_[bucket_idx].find(fingerprint) != buckets_[bucket_idx].end()) {
-    buckets_[bucket_idx].erase(fingerprint);
-    return true;
-  } else {
-    return false;
+  std::vector<uint32_t> bucket = buckets_[bucket_idx];
+
+  for (size_t i; i < bucket.size(); i++) {
+    if (bucket[i] == fingerprint) {
+      bucket.erase(bucket.begin() + i);
+      return true;
+    }
   }
+  return false;
 }
 
 bool CuckooHashTable::Contains(uint32_t fingerprint, size_t bucket_idx) {
   bucket_idx %= this->NumBuckets();
-  return buckets_[bucket_idx].find(fingerprint) != buckets_[bucket_idx].end();
+  std::vector<uint32_t> bucket = buckets_[bucket_idx];
+
+  for (auto entry : bucket) {
+    if (entry == fingerprint) {
+      return true;
+    }
+  }
+  return false;
 }
 
 uint32_t CuckooHashTable::SwapEntries(uint32_t fingerprint, size_t bucket_idx) {
   bucket_idx %= this->NumBuckets();
-  std::vector<uint32_t> bucket_cpy(buckets_[bucket_idx].size());
-  std::copy(buckets_[bucket_idx].begin(), buckets_[bucket_idx].end(),
-            bucket_cpy.begin());
+  std::vector<uint32_t> bucket = buckets_[bucket_idx];
 
-  // random generator
-  std::random_device rd;  // only used once to initialise (seed) engine
-  std::mt19937 rng(
-      rd());  // random-number engine used (Mersenne-Twister in this case)
-  std::uniform_int_distribution<int> uni(
-      0, buckets_[bucket_idx].size() - 1);  // guaranteed unbiased
-  size_t swap_idx = uni(rng);
-
-  uint32_t old_entry = bucket_cpy[swap_idx];
-  buckets_[bucket_idx].erase(old_entry);
-  buckets_[bucket_idx].insert(fingerprint);
+  uint32_t old_entry = bucket.at(0);
+  bucket.at(0) = fingerprint;
   return old_entry;
 }
 }  // namespace cuckoofilter
