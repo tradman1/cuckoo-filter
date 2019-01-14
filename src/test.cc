ee4b0dc7dba264f6ddc9fa9e128181dde73e8181
tradman1 <tome.radman95@gmail.com>
2019-01-14 20:13:10 +0100

    Change fingerprint type from uint32_t to uint16_t

--- src/test.cc
+++ src/test.cc
@@ -7,49 +7,49 @@
 
 using namespace cuckoofilter;
 
-// int main() {
-//   size_t total_items = 1000000;
-//   CuckooFilter<size_t> filter(total_items);
+int main() {
+  size_t total_items = 1000;
+  CuckooFilter<size_t> filter(total_items);
 
-//   // Insert items to this cuckoo filter
-//   size_t num_inserted = 0;
-//   for (size_t i = 0; i < total_items; i++, num_inserted++) {
-//     std::cout << "inserting item " << i << std::endl;
-//     if (filter.Insert(i) != Ok) {
-//       std::cout << "inserted " << 100.0 * num_inserted / total_items
-//                 << "% items %\n"
-//                 << std::endl;
-//       break;
-//     }
-//   }
+  // Insert items to this cuckoo filter
+  size_t num_inserted = 0;
+  for (size_t i = 0; i < total_items; i++, num_inserted++) {
+    std::cout << "inserting item " << i << std::endl;
+    if (filter.Insert(i) != Ok) {
+      std::cout << "inserted " << 100.0 * num_inserted / total_items
+                << "% items %\n"
+                << std::endl;
+      break;
+    }
+  }
 
-//   assert(num_inserted >= 1);
+  assert(num_inserted >= 1);
 
-//   std::cout << "Print table" << std::endl;
-//   filter.PrintTable();
+  std::cout << "Print table" << std::endl;
+  filter.PrintTable();
 
-//   // Check if previously inserted items are in the filter, expected
-//   // true for all items
-//   for (size_t i = 0; i < num_inserted; i++) {
-//     std::cout << "looking for item " << i << std::endl;
-//     assert(filter.Lookup(i) == Ok);
-//   }
+  // Check if previously inserted items are in the filter, expected
+  // true for all items
+  for (size_t i = 0; i < num_inserted; i++) {
+    std::cout << "looking for item " << i << std::endl;
+    assert(filter.Lookup(i) == Ok);
+  }
 
-//   // Check non-existing items, a few false positives expected
-//   size_t total_queries = 0;
-//   size_t false_queries = 0;
-//   for (size_t i = total_items; i < 2 * total_items; i++) {
-//     if (filter.Lookup(i) == Ok) {
-//       false_queries++;
-//     }
-//     total_queries++;
-//   }
+  // Check non-existing items, a few false positives expected
+  size_t total_queries = 0;
+  size_t false_queries = 0;
+  for (size_t i = total_items; i < 2 * total_items; i++) {
+    if (filter.Lookup(i) == Ok) {
+      false_queries++;
+    }
+    total_queries++;
+  }
 
-//   // Output the measured false positive rate
-//   std::cout << "false positive rate is "
-//             << 100.0 * false_queries / total_queries << "%\n";
+  // Output the measured false positive rate
+  std::cout << "false positive rate is "
+            << 100.0 * false_queries / total_queries << "%\n";
 
-//   std::cout << "PASSED THE TEST!!!" << std::endl;
+  std::cout << "PASSED THE TEST!!!" << std::endl;
 
-//   return 0;
-// }
+  return 0;
+}
