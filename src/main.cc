34f94622ed12e6debc3f59255eddbafefe9c294a
I J <iva.jurkovic@fer.hr>
2019-01-14 10:21:53 -0800

    Doraden main

---
+++ src/main.cc
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <functional>
+#include <algorithm> 
+#include <assert.h>
+#include "cuckoo_filter.h"
+#include "cuckoo_hasher.h"
+#include "cuckoo_hashtable.h"
+#include "cuckoo_filter.cc"
+
+using namespace std;
+
+using namespace cuckoofilter;
+
+ofstream outFile;
+
+size_t total_items;
+size_t bits = 12;
+
+//upload string from file 
+string upload_genome(string path)
+{
+	string genome = "";
+	string line;
+	ifstream myfile;
+	myfile.open(path, ios::in);
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			genome += line;
+		}
+		myfile.close();
+	}
+	else cout << "Unable to open file";
+	return genome;
+}
+
+std::string generate_random_string (int length)
+{
+    auto randchar = []() -> char
+    {
+        const char charset[] =
+        "ACGT";
+        const size_t max_index = (sizeof(charset) - 1);
+        return charset[ rand() % max_index ];
+    };
+    std::string str(length,0);
+    std::generate_n( str.begin(), length, randchar );
+    return str;
+}
+
+int main(int argc, char* argv[])
+{
+	//upload_genome("D:\\3.semestar\\BIOINF\\coli\\GCF_000005845.2_ASM584v2_genomic.fna");
+	int k = atoi(argv[2]);
+    int algorithm = atoi(argv[3]);
+	//string genome = upload_genome("/home/jiva26/Documents/bioinf/GCF_000005845.2_ASM584v2_genomic.fna");
+	string genome = upload_genome(argv[1]);
+	total_items = genome.length() - k + 1;
+	outFile.open("results.txt", ios::out | ios::app );
+	outFile << "New test...uploaded genome from file: " << argv[1] << ", k = " << k << ", total_items = " << total_items << "algorithm = " << algorithm <<endl;
+	CuckooFilter<std::string> filter(total_items, bits, Hash::STL);
+    //Fill Cuckoo Filtar
+    size_t num_inserted = 0;
+	for (size_t i = 0; i < total_items; i++, num_inserted++)
+	{
+		std::string k_substring = genome.substr(i, k);
+		//std::cout << k_substring << "\n";
+		if (filter.Insert(k_substring) != Ok)
+		{
+			outFile << "Inserted " << 100.0 * num_inserted / total_items << "% items" << endl;
+			break;
+		}
+	}
+	//Check if string is in filtar
+    size_t false_queries = 0;
+	srand(time(0));
+	//check if in cuckoo filtar
+	for (size_t i = 0; i < total_items; i++) {
+        std::string random_string;
+        if (algorithm == 1) {
+            random_string = genome.substr((rand() % (genome.length() - k + 1) + 0), k);
+        } else {
+            random_string = generate_random_string(k);
+        }
+		if (filter.Lookup(random_string) == Ok) 
+		{
+			false_queries++;
+    	}
+	}
+	outFile << "False positive rate = " << 100.0 * false_queries / total_items << "%\n";
+	return 0;
+}
